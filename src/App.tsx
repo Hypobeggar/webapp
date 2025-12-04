b60983a9a17c0cab0d4da8285e151461771a0371
Hypobeggar <89600337+Hypobeggar@users.noreply.github.com>
2025-12-04 13:27:40 -0600

    Update for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import PlainResume from './PlainResume';
 import MainResume from './MainResume';
 
